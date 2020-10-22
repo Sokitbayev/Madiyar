1cb737fa066dd1f49a6d2781df664236246c1cc2
Sokitbayev Meirzhan <46749286+Sokitbayev@users.noreply.github.com>
2020-10-23 01:21:37 +0600

    Add files via upload

---
+++ Madiyar/app/src/main/java/com/example/madiyar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.madiyar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}

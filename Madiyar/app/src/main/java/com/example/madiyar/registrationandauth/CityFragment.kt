f605ee8b5afe8972140af1d714759b3462d672a0
Sokitbayev <180107278@stu.sdu.edu.kz>
2020-10-23 19:41:59 +0600

    Refactor names

---
+++ Madiyar/app/src/main/java/com/example/madiyar/registrationandauth/CityFragment.kt
@@ -0,0 +1,19 @@
+package com.example.madiyar.registrationandauth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.madiyar.R
+
+
+class CityFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_city,container,false)
+    }
+}

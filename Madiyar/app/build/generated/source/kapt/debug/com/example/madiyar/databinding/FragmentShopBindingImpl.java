8ea106f2a8ce1e8b9d1a1c0dd1d8917ebcbfa06e
Sokitbayev <Sokitbayev@gmail.com>
2020-12-13 08:54:40 +0600

    save

--- Madiyar/app/build/generated/source/kapt/debug/com/example/madiyar/databinding/FragmentShopBindingImpl.java
+++ Madiyar/app/build/generated/source/kapt/debug/com/example/madiyar/databinding/FragmentShopBindingImpl.java
@@ -28,37 +28,95 @@ public class FragmentShopBindingImpl extends FragmentShopBinding  {
         sViewsWithIds.put(R.id.imageView14, 12);
         sViewsWithIds.put(R.id.imageView15, 13);
         sViewsWithIds.put(R.id.btn_basket, 14);
+        sViewsWithIds.put(R.id.linearLayout, 15);
+        sViewsWithIds.put(R.id.textView101, 16);
+        sViewsWithIds.put(R.id.cv_apple, 17);
+        sViewsWithIds.put(R.id.imageView20, 18);
+        sViewsWithIds.put(R.id.textView102, 19);
+        sViewsWithIds.put(R.id.textView103, 20);
+        sViewsWithIds.put(R.id.textView104, 21);
+        sViewsWithIds.put(R.id.cv_orange, 22);
+        sViewsWithIds.put(R.id.imageView201, 23);
+        sViewsWithIds.put(R.id.textView1021, 24);
+        sViewsWithIds.put(R.id.textView1031, 25);
+        sViewsWithIds.put(R.id.textView1041, 26);
+        sViewsWithIds.put(R.id.imageView22, 27);
+        sViewsWithIds.put(R.id.bottom_sheet, 28);
+        sViewsWithIds.put(R.id.textView97, 29);
+        sViewsWithIds.put(R.id.textView98, 30);
+        sViewsWithIds.put(R.id.textView99, 31);
+        sViewsWithIds.put(R.id.cardView33, 32);
+        sViewsWithIds.put(R.id.cardView2, 33);
+        sViewsWithIds.put(R.id.cardView3, 34);
+        sViewsWithIds.put(R.id.textView100, 35);
+        sViewsWithIds.put(R.id.button2, 36);
+        sViewsWithIds.put(R.id.button3, 37);
+        sViewsWithIds.put(R.id.textView105, 38);
+        sViewsWithIds.put(R.id.imageView19, 39);
+        sViewsWithIds.put(R.id.button6, 40);
+        sViewsWithIds.put(R.id.imageView1812, 41);
+        sViewsWithIds.put(R.id.textView106, 42);
+        sViewsWithIds.put(R.id.textView107, 43);
+        sViewsWithIds.put(R.id.btn_add, 44);
     }
     // views
-    @NonNull
-    private final androidx.core.widget.NestedScrollView mboundView0;
     // variables
     // values
     // listeners
     // Inverse Binding Event Handlers
 
     public FragmentShopBindingImpl(@Nullable androidx.databinding.DataBindingComponent bindingComponent, @NonNull View root) {
-        this(bindingComponent, root, mapBindings(bindingComponent, root, 15, sIncludes, sViewsWithIds));
+        this(bindingComponent, root, mapBindings(bindingComponent, root, 45, sIncludes, sViewsWithIds));
     }
     private FragmentShopBindingImpl(androidx.databinding.DataBindingComponent bindingComponent, View root, Object[] bindings) {
         super(bindingComponent, root, 0
+            , (androidx.coordinatorlayout.widget.CoordinatorLayout) bindings[28]
+            , (android.widget.Button) bindings[44]
             , (android.widget.Button) bindings[14]
+            , (android.widget.Button) bindings[36]
+            , (android.widget.Button) bindings[37]
+            , (android.widget.Button) bindings[40]
+            , (androidx.cardview.widget.CardView) bindings[33]
+            , (androidx.cardview.widget.CardView) bindings[34]
+            , (androidx.cardview.widget.CardView) bindings[32]
             , (androidx.cardview.widget.CardView) bindings[2]
+            , (androidx.cardview.widget.CardView) bindings[17]
+            , (androidx.cardview.widget.CardView) bindings[22]
             , (android.widget.ImageView) bindings[6]
             , (android.widget.ImageView) bindings[8]
             , (android.widget.ImageView) bindings[10]
             , (android.widget.ImageView) bindings[12]
             , (android.widget.ImageView) bindings[13]
+            , (android.widget.ImageView) bindings[41]
+            , (android.widget.ImageView) bindings[39]
+            , (android.widget.ImageView) bindings[18]
+            , (android.widget.ImageView) bindings[23]
+            , (android.widget.ImageView) bindings[27]
+            , (android.widget.LinearLayout) bindings[15]
             , (android.widget.RatingBar) bindings[5]
             , (com.google.android.material.textfield.TextInputLayout) bindings[1]
+            , (android.widget.TextView) bindings[35]
+            , (android.widget.TextView) bindings[16]
+            , (android.widget.TextView) bindings[19]
+            , (android.widget.TextView) bindings[24]
+            , (android.widget.TextView) bindings[20]
+            , (android.widget.TextView) bindings[25]
+            , (android.widget.TextView) bindings[21]
+            , (android.widget.TextView) bindings[26]
+            , (android.widget.TextView) bindings[38]
+            , (android.widget.TextView) bindings[42]
+            , (android.widget.TextView) bindings[43]
             , (android.widget.TextView) bindings[3]
             , (android.widget.TextView) bindings[4]
             , (android.widget.TextView) bindings[7]
             , (android.widget.TextView) bindings[9]
             , (android.widget.TextView) bindings[11]
+            , (android.widget.TextView) bindings[29]
+            , (android.widget.TextView) bindings[30]
+            , (android.widget.TextView) bindings[31]
+            , (androidx.coordinatorlayout.widget.CoordinatorLayout) bindings[0]
             );
-        this.mboundView0 = (androidx.core.widget.NestedScrollView) bindings[0];
-        this.mboundView0.setTag(null);
+        this.userMainPageFragment.setTag(null);
         setRootTag(root);
         // listeners
         invalidateAll();
--- Madiyar/app/src/main/java/com/example/madiyar/ordering/ShopFragment.kt
+++ Madiyar/app/src/main/java/com/example/madiyar/ordering/ShopFragment.kt
@@ -1,15 +1,15 @@
 package com.example.madiyar.ordering
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.madiyar.R
-import com.example.madiyar.databinding.FragmentAddressBinding
 import com.example.madiyar.databinding.FragmentShopBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
 class ShopFragment : Fragment() {
@@ -19,10 +19,26 @@ class ShopFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentShopBinding>(inflater,
-            R.layout.fragment_shop,container,false)
+        val binding = DataBindingUtil.inflate<FragmentShopBinding>(
+            inflater,
+            R.layout.fragment_shop, container, false
+        )
+        val bottomSheetBehavior: BottomSheetBehavior<*> =
+            BottomSheetBehavior.from<View>(binding.bottomSheet)
         binding.btnBasket.setOnClickListener {
             it.findNavController().navigate(R.id.action_shopFragment_to_basketFragment)
+        }
+        binding.cvApple.setOnClickListener {
+
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+        }
+        binding.cvOrange.setOnClickListener {
+
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+        }
+        binding.btnAdd.setOnClickListener {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN)
+
         }
         return binding.root
     }
